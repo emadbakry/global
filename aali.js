64aeaea639584e404e65ae2f32550d0a2d8539d9
Emad ALbakri <101658737+emadbakry@users.noreply.github.com>
2023-11-28 12:25:52 +0300

    fix header burger icon color for all store

--- aali.js
+++ aali.js
@@ -1,1 +1,18 @@
 // you can use this file to fix issues directly for all theme users, great for quick fixes before salla accept fixes.
+
+// add style element
+var style = document.createElement("style");
+// add type using setAttribute
+
+style.setAttribute("type", "text/css");
+
+// add css content
+
+style.innerHTML = `
+.store-header .mburger i {
+    color: var(--header-text-color);
+}
+`;
+// append style element
+
+document.getElementsByTagName("head")[0].appendChild(style);
