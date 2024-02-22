62cadd4980fa5790ea2d97bd884c9a5bc822055c
Emad ALbakri <101658737+emadbakry@users.noreply.github.com>
2024-02-23 01:01:39 +0300

    fix height of fs banners cuz class priorty changed

--- aali.js
+++ aali.js
@@ -16,6 +16,30 @@ style.innerHTML = `
 html:not(.dark) .product-single .total-price.t-red {
 	color: var(--red-color,#ef4444);
 }
+
+
+.index#app #fullscreen-banner-0 .bg__image {
+    height: 75vh;
+    height: 80vh;
+    height: var(--fs-desk-height,80vh);
+    min-height: 20vh
+}
+
+@media only screen and (min-width: 492px) and (max-width:1024px) {
+    .index#app #fullscreen-banner-0 .bg__image {
+        height:90vh;
+        height: var(--fs-tablet-height,90vh);
+        min-height: 20vh
+    }
+}
+
+@media only screen and (max-width: 492px) {
+    .index#app #fullscreen-banner-0 .bg__image {
+        height:90vh;
+        height: var(--fs-mob-height,90vh);
+        min-height: 20vh
+    }
+}
 			
 `;
 // append style element
