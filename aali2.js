e04f2cdb25dd879e17a69ff7dbcb82c9526dab71
emad bakri <a@jahmi.sa>
2024-12-25 10:59:30 +0300

    added video

--- aali2.js
+++ aali2.js
@@ -315,7 +315,7 @@ body.modal-is-closed salla-button#cart-submit button .s-button-loader {
 	.sticky-product-bar .sticky-product-bar__price {
 		display: none !important;
 	}
-	#app.is-sticky-product-bar .sticky-bar-price-on .sticky-product-bar__quantity {
+	#app.is-sticky-product-bar .sticky-product-bar__quantity {
 		width: 100%;
 	}
 }
