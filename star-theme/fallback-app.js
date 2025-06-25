a24c876c35ec9a876ac91981c8a656b82452f681
emad bakri <a@jahmi.sa>
2025-06-25 17:59:58 +0300

    star: fix scrolling in bottom menus

--- star-theme/fallback-app.js
+++ star-theme/fallback-app.js
@@ -1,2 +1,19 @@
 // hi!, this i fallback script file to fix problems of theme directly when URGANT!
 console.log("hi!");
+
+let style = document.createElement("style");
+
+style.innerHTML = `
+.mm-ocd__content:has(.brands-menu) {
+  overflow: unset;
+}
+ 
+#offers-menu.h-screen {
+  height: auto;
+}
+
+div.swal2-container {
+  z-index: 99999;
+}`;
+
+document.getElementsByTagName("head")[0].appendChild(style);
