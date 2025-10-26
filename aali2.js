e79b91c12b913e37f0cb60d4e4186016b0c49564
emad bakri <a@jahmi.sa>
2025-10-26 09:52:13 +0300

    fix review or salla-comments hidden style when no reviews and comments on

--- aali2.js
+++ aali2.js
@@ -565,7 +565,7 @@ form textarea {
 /* product single */
 
 
-body .s-comments.s-comments-product:has(salla-reviews-summary:empty):has(.s-comments-header:empty) {
+body .s-comments.s-comments-product:has(salla-reviews-summary:empty):has(.s-comments-header:empty):has(salla-comment-form:empty) {
     display: none;
 }
 
