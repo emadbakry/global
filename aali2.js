d74a9797876d55a21b82174f40f12bb2a33924b1
emad bakri <a@jahmi.sa>
2025-11-23 15:04:54 +0300

    fix footer title too

--- aali2.js
+++ aali2.js
@@ -850,8 +850,12 @@ document.addEventListener("DOMContentLoaded", function () {
 	console.log(`Updated ${deleteButtons.length} cart delete buttons`);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+function fix_footer_title() {
 	let footer_contact_title = document.querySelector(".footer-contact .hidden .s-contacts-title");
 	if (footer_contact_title) return;
 	footer_contact_title.innerHTML = footer_contact_title.innerHTML.split("hide-title=")[0];
-});
+}
+document.addEventListener("DOMContentLoaded", fix_footer_title);
+setTimeout(() => {
+	fix_footer_title();
+}, 2000);
